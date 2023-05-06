05b4cc721daadb84139c4f6ea296784dc8d7c422
Hara Gopal Kovvali <haragopal.kovvali@gmail.com>
2023-05-07 00:54:59 +0200

    Add project files.

---
+++ RazorPagesTour/Pages/Privacy.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesTour.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            //handles the logic for all get requests to this page
+            _logger.LogInformation("Privacy page visited.");
+            Console.WriteLine("privacy page is clicked");
+        }
+    }
+}
