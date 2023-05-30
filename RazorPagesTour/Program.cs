3a9c7a9232d94e8b707fd9c256b7c7acbcdfef6d
Hara Gopal Kovvali <haragopal.kovvali@gmail.com>
2023-05-30 18:44:51 +0200

    pluralsight

--- RazorPagesTour/Program.cs
+++ RazorPagesTour/Program.cs
@@ -6,13 +6,10 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
-        builder.Services.AddRazorPages();
-
+        builder.Services.AddRazorPages(); // Add services to the container.
         var app = builder.Build();
-
-        // Configure the HTTP request pipeline (MIDDLEWARE COMPONENTS)
-        if (!app.Environment.IsDevelopment())
+                
+        if (!app.Environment.IsDevelopment()) // Configure the HTTP request pipeline (MIDDLEWARE COMPONENTS)
         {
             app.UseExceptionHandler("/Error");
             // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
@@ -24,9 +21,7 @@ public class Program
         app.UseRouting();
         app.UseHttpLogging();
         app.UseAuthorization();
-        app.MapRazorPages(); 
-        
-        
+        app.MapRazorPages();
         app.Run();
     }
 }
